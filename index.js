849c52005e53997275f834c6bd986a04a1f1374a
Lexy Perry <lexy.perry61022@gmail.com>
2025-05-15 22:44:25 -0400

    finished

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-
-
-
+function combineUsers(...args){
+ let combineObject = {
+  users: [], merge_date: ''
+ };
+   for (let arr of args) {
+    combineObject.users.push(...arr);
+  }combineObject.merge_date = new Date().toString('M/d/yyyy');
+  return combineObject;
+}
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
